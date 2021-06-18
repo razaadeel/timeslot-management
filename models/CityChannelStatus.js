ad472804d14124b2c78d538453a86cf0dc5a03eb
Adeel Raza <teamouts002@gmail.com>
2021-06-18 05:38:52 +0500

    check video length, CityChannelStatus table update

--- models/CityChannelStatus.js
+++ models/CityChannelStatus.js
@@ -1,13 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
     const CityChannelStatus = sequelize.define('CityChannelStatus', {
+        cityId: {
+            type: DataTypes.INTEGER
+        },
         channelName: {
             type: DataTypes.STRING
         },
         status: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            defaultValue: 'offline'
         },
         scheduling: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            defaultValue: 'manual'
         },
     }, {
         timestamps: true,
