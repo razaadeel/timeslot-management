32afd10c76c133c865110b10cdf4bf0e4a04d6ce
teamouts002 <teamouts002@gmail.com>
2021-07-20 04:09:27 +0500

    testing callback url

--- services/qencode.js
+++ services/qencode.js
@@ -121,7 +121,7 @@ exports.manualSystem = async (videoUrl, destination, outputVideoName, userId, vi
                 }
             ],
             encoder_version: "2",
-            callback_url: `https://210b84b820cf.ngrok.io/api/video/qencode-request?destination=${destination}&outputvideoname=${outputVideoName}&userId=${userId}&videoId=${videoId}`,
+            callback_url: `http://tms-dev01.services.citystvnetwork.com/api/video/qencode-request?destination=${destination}&outputvideoname=${outputVideoName}&userId=${userId}&videoId=${videoId}`,
             // callback_url: `https://citystreamingtelevision.com/api/video/qencode-request?destination=${destination}&outputvideoname=${outputVideoName}&userId=${userId}&videoId=${videoId}`,
             source: videoUrl
         }
@@ -250,7 +250,7 @@ exports.transcodeAdVideo = async (videoUrl, destinations, duration, campaignId) 
                 }
             ],
             encoder_version: "2",
-            callback_url: `https://210b84b820cf.ngrok.io/api/video/callback/advideo-qencode-status?campaignId=${campaignId}`,
+            callback_url: `http://tms-dev01.services.citystvnetwork.com/api/video/callback/advideo-qencode-status?campaignId=${campaignId}`,
             // callback_url: `https://citystreamingtelevision.com/api/video/callback/advideo-qencode-status?campaignId=${campaignId}`,
             source: videoUrl,
         }
