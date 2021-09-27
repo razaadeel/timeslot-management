fc697934ff066de0e1fd1ce91ed41adc37b67ac9
Adeel Raza <teamouts002@gmail.com>
2021-09-27 16:51:04 +0500

    AdsReporting bug fix

--- models/AdsReporting.js
+++ models/AdsReporting.js
@@ -1,6 +1,8 @@
 'use strict';
-
-const { Model } = require('sequelize');
+const {
+    Model,
+    Op
+} = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
     class AdsReporting extends Model {
